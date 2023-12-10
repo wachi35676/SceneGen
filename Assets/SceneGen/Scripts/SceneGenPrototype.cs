60f7fe0cdc6eab753a9d1e9844c36f3500f7a4fe
Muhammad Wasif Ali Wasif <wasif35676@gmail.com>
2023-12-10 15:25:37 +0500

    added tress

--- Assets/SceneGen/Scripts/SceneGenPrototype.cs
+++ Assets/SceneGen/Scripts/SceneGenPrototype.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using SceneGen.Scripts;
 using UnityEngine;
 using SceneGen.Scripts;
@@ -44,6 +45,17 @@ public class SceneGenPrototype : MonoBehaviour
     public GameObject CornerGrass;
     public GameObject CornerGrassWide;
     public GameObject CornerGrassHigh;
+    
+    [Header("(Optional) Add Tree")]
+    public Dictionary<char, string> rules = new Dictionary<char, string>();
+    [Range(0,6)]
+    public int iterations = 4;
+    public string input = "F";
+    public float angle = 30.0f;
+    public string rule = "F[-F]F[+F][F]";
+    private string output;
+    
+    public GameObject cylinder;
     
     [Header("(Optional) Add Bridge")]
     public GameObject Bridge;
@@ -126,6 +138,7 @@ public class SceneGenPrototype : MonoBehaviour
     
 public void SceneGeneration()
 {
+    
     // Generate a random width for the biome
     float width = UnityEngine.Random.Range(MinBiomeSize, MaxBiomeSize);
 
@@ -228,6 +241,15 @@ public void SceneGeneration()
         for (int j = 0; j < h; j++)
         {
             Spawn(Dirt, new Vector3(i + offset, j, 0), Quaternion.identity);
+        }
+        
+        if (cylinder != null)
+        {
+            // Generate tree
+            if (UnityEngine.Random.Range(0, 100) < 10)
+            {
+                GenerateTree(i + offset, height);
+            }
         }
        
         
@@ -408,5 +430,138 @@ private int GeneratePlatforms(int i, int h, bool isBridge = false)
     int offset = i - starting;
     return offset;
 }
+
+public void GenerateTree(int x, int y)
+    {
+        List<point> points = new List<point>();
+        List<GameObject> branches = new List<GameObject>();
+        
+        rules.Clear();
+
+        // Rules
+        // Key is replaced by value
+        rules.Add('F', rule);
+
+        // Apply rules for i interations
+        output = input;
+        for (int i = 0; i < iterations; i++)
+        {
+            output = applyRules(output);
+        }
+        determinePoints(output, points);
+        CreateCylinders(points, branches, x, y);
+    }
+
+    string applyRules(string p_input)
+    {
+        StringBuilder sb = new StringBuilder();
+        // Loop through characters in the input string
+        foreach (char c in p_input)
+        {
+            // If character matches key in rules, then replace character with rhs of rule
+            if (rules.ContainsKey(c))
+            {
+                sb.Append(rules[c]);
+            }
+            // If not, keep the character
+            else
+            {
+                sb.Append(c);
+            }
+        }
+        // Return string with rules applied
+        return sb.ToString();
+    }
+
+    struct point
+    {
+        public point(Vector3 rP, Vector3 rA, float rL) { Point = rP; Angle = rA; BranchLength = rL; }
+        public Vector3 Point;
+        public Vector3 Angle;
+        public float BranchLength;
+    }
+
+    void determinePoints(string p_input, List<point> points)
+    {
+        Stack<point> returnValues = new Stack<point>();
+        point lastPoint = new point(Vector3.zero, Vector3.zero, 1f);
+        returnValues.Push(lastPoint);
+
+        foreach (char c in p_input)
+        {
+            switch (c)
+            {
+                case 'F': // Draw line of length lastBranchLength, in direction of lastAngle
+                    points.Add(lastPoint);
+
+                    point newPoint = new point(lastPoint.Point + new Vector3(0, lastPoint.BranchLength, 0), lastPoint.Angle, 1f);
+                    newPoint.BranchLength = lastPoint.BranchLength - 0.02f;
+                    if (newPoint.BranchLength <= 0.0f) newPoint.BranchLength = 0.001f;
+
+                    newPoint.Angle.y = lastPoint.Angle.y + UnityEngine.Random.Range(-30, 30);
+
+                    newPoint.Point = pivot(newPoint.Point, lastPoint.Point, new Vector3(newPoint.Angle.x, 0, 0));
+                    newPoint.Point = pivot(newPoint.Point, lastPoint.Point, new Vector3(0, newPoint.Angle.y, 0));
+
+                    points.Add(newPoint);
+                    lastPoint = newPoint;
+                    break;
+                case '+': // Rotate +30
+                    lastPoint.Angle.x += angle;
+                    break;
+                case '[': // Save State
+                    returnValues.Push(lastPoint);
+                    break;
+                case '-': // Rotate -30
+                    lastPoint.Angle.x += -1 * angle;
+                    break;
+                case ']': // Load Saved State
+                    lastPoint = returnValues.Pop();
+                    break;
+            }
+        }
+    }
+
+    void CreateCylinders(List<point> points, List<GameObject> branches, int x, int y)
+    {
+        //make a tree game object
+        GameObject tree = new GameObject();
+        tree.name = "Tree";
+        tree.transform.parent = transform;
+        for (int i = 0; i < points.Count; i += 2)
+        {
+            CreateCylinder(points[i], points[i + 1], 0.1f, branches, tree);
+        }
+        
+        tree.transform.position = new Vector3(x, y, 0);
+    }
+
+    // Pivot point1 around point2 by angles
+    Vector3 pivot(Vector3 point1, Vector3 point2, Vector3 angles)
+    {
+        Vector3 dir = point1 - point2;
+        dir = Quaternion.Euler(angles) * dir;
+        point1 = dir + point2;
+        return point1;
+    }
+
+    void CreateCylinder(point point1, point point2, float radius, List<GameObject> branches, GameObject tree)
+    {
+        //UnityEngine.Random.Range(0,3);
+        GameObject newCylinder = (GameObject)Instantiate(cylinder);
+        newCylinder.SetActive(true);
+        float length = Vector3.Distance(point2.Point, point1.Point);
+        radius = radius * length;
+
+        Vector3 scale = new Vector3(radius, length / 2.0f, radius);
+        newCylinder.transform.localScale = scale;
+
+        newCylinder.transform.position = point1.Point;
+        newCylinder.transform.Rotate(point2.Angle);
+
+        newCylinder.transform.parent = tree.transform;
+
+        branches.Add(newCylinder);
+    }
 
 }
