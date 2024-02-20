b091e1861e5f9400d24c40e6e5537fd736360870
wachi <muhammad.wasif@questlab.pk>
2024-02-20 15:31:57 +0500

    starting biome transition

---
+++ Assets/SceneGen/Scripts/BiomeTransistionGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(BiomeTransition))]
+public class BiomeTransistionGenerator:Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector() ;
+        BiomeTransition myScript = (BiomeTransition)target;
+        if (GUILayout.Button("Generate"))
+        {
+            myScript.Generate();
+        }
+    }
+}
---
+++ Assets/SceneGen/Scripts/BiomeTransition.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BiomeTransition : MonoBehaviour
+{
+    public GameObject[] biomes;
+
+    public void Generate()
+    {
+        
+    }
+}
