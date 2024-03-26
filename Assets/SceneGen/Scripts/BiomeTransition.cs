39125720aacf439349d616a220ff29d87a307293
Muhammad Wasif Ali Wasif <muhammad.wasif@questlab.pk>
2024-03-26 18:57:38 +0500

    Update BiomeTransition.cs

--- Assets/SceneGen/Scripts/BiomeTransition.cs
+++ Assets/SceneGen/Scripts/BiomeTransition.cs
@@ -13,6 +13,9 @@ public void Generate()
     for (int i = 0; i < biomes.Length; i++)
     {
         GameObject biome = biomes[i];
+        // set starting x position of biome to 0
+        biome.transform.position = new Vector3(0, biome.transform.position.y, biome.transform.position.z);
+        biome.GetComponent<SceneGenPrototype>().Generate();
         float width = biome.GetComponent<SceneGenPrototype>().TotalWidth;
         Vector3 newPosition = new Vector3(currentPosition, biome.transform.position.y, biome.transform.position.z);
         biome.transform.position = newPosition;
