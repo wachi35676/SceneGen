d7a9b00c98b6c53de2f9e4571a0213b44c943dca
Sania Arshad <i202425@nu.edu.pk>
2024-04-23 22:23:46 +0500

    Added sprites, animator and jump,idle,run animations in script

--- Assets/SceneGen/Scripts/PlayerController.cs
+++ Assets/SceneGen/Scripts/PlayerController.cs
@@ -4,42 +4,49 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
     public float speed = 10.0f;
     public float jumpHeight = 10.0f;
-    
+
     private Rigidbody2D _rigidbody2D;
-    
+    private Animator _animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_rigidbody2D == null)
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        _rigidbody2D.velocity = new Vector2(moveInput * speed, _rigidbody2D.velocity.y);
+
+        // Flip character sprite based on movement direction
+        if (moveInput > 0)
         {
-            _rigidbody2D = GetComponent<Rigidbody2D>();
+            transform.localScale = new Vector3(1f, 1f, 1f);
         }
-        
-        if (Input.GetKey(KeyCode.A))
+        else if (moveInput < 0)
         {
-            _rigidbody2D.velocity = new Vector2(-speed, _rigidbody2D.velocity.y);
+            transform.localScale = new Vector3(-1f, 1f, 1f);
         }
-        else if (Input.GetKey(KeyCode.D))
+
+        // Set animation parameters
+        if (Mathf.Abs(moveInput) > 0)
         {
-            _rigidbody2D.velocity = new Vector2(speed, _rigidbody2D.velocity.y);
+            _animator.SetBool("isRunning", true);
         }
         else
         {
-            _rigidbody2D.velocity = new Vector2(0, _rigidbody2D.velocity.y);
+            _animator.SetBool("isRunning", false);
         }
-        
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, jumpHeight);
+            _animator.SetTrigger("Jump");
         }
     }
 }
